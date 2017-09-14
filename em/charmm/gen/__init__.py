c49b7314e76f0665a0790af8210c9296d85fd3b6
Bartlomiej Lewandowski <blewandowski@atlassian.com>
2017-09-13 20:29:47 -0700

    templating

---
+++ em/charmm/gen/__init__.py
@@ -0,0 +1,33 @@
+from jinja2 import Environment, PackageLoader
+
+env = Environment(
+    loader=PackageLoader('em.charmm.gen', 'templates')
+)
+
+
+def gen_setup_one():
+    template = env.get_template('setup_one.inp.template')
+    a_pair = {
+        'id': 'A',
+        'seq': "A.SEQ",
+        'first': 'none',
+        'last': 'none',
+        'inp': "A_FIXRES.INP"
+    }
+    b_pair = {
+        'id': 'B',
+        'seq': "B.SEQ",
+        'first': 'none',
+        'last': 'none',
+        'inp': "B_FIXRES.INP"
+    }
+
+    return template.render(
+        param_absolute_path='/code/em/params/charmm27.ff/',
+        a_sequence=a_pair,
+        b_sequence=b_pair,
+        in_filename='INFILE',
+        out_filename='OUTFILE')
+
+if __name__ == '__main__':
+    gen_setup_one()
